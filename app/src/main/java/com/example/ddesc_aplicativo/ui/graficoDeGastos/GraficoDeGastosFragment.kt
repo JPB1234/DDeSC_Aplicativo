ac1e5ca4e71805f406c4c89fc4431830f4181fa6
Lucas Maya <lukasmayaa@gmail.com>
2022-11-27 16:32:31 -0300

    Tela de Grafico de Gastos implementada no menu

--- app/src/main/java/com/example/ddesc_aplicativo/ui/graficoDeGastos/GraficoDeGastosFragment.kt
+++ app/src/main/java/com/example/ddesc_aplicativo/ui/graficoDeGastos/GraficoDeGastosFragment.kt
@@ -1,11 +1,15 @@
 package com.example.ddesc_aplicativo.ui.graficoDeGastos
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.ddesc_aplicativo.databinding.FragmentGraficoDeGastosBinding
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.*
+import com.github.mikephil.charting.utils.ColorTemplate
 
 class GraficoDeGastosFragment : Fragment() {
     private lateinit var binding: FragmentGraficoDeGastosBinding
@@ -23,9 +27,34 @@ class GraficoDeGastosFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.txtGraficoDeGastos.text = "Gráfico de seus gastos"
+
+        val list: ArrayList<PieEntry> = ArrayList()
+
+        list.add((PieEntry(100f, "100")))
+        list.add((PieEntry(101f, "101")))
+        list.add((PieEntry(102f, "102")))
+        list.add((PieEntry(103f, "103")))
+        list.add((PieEntry(104f, "104")))
+
+        val pieDataSet= PieDataSet(list, "List")
+
+        pieDataSet.setColors(ColorTemplate.MATERIAL_COLORS, 255)
+
+        pieDataSet.valueTextSize=15f
+
+        pieDataSet.valueTextColor= Color.BLACK
+
+        val pieData= PieData(pieDataSet)
+
+        binding.pieChart.data = pieData
+
+        binding.pieChart.description.text="Pie Chart"
+
+        binding.pieChart.centerText="List"
+
+        binding.pieChart.animateY(2000)
     }
 
-    }
+}
 
 
